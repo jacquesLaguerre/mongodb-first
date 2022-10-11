8d4f8f1cf0fd393884e2132cb90b1c61c33ecf21
Jacques Laguerre <jacquesblaguerre@gmail.com>
2022-10-11 16:03:30 -0400

    insert movie in db

--- mongodb.js
+++ mongodb.js
@@ -2,22 +2,37 @@ import {MongoClient} from 'mongodb'
 
 import{uri } from "./credentials.js"
 
-const client = new MongoClient(uri)
+const client = new MongoClient(uri)//how to connect to database server 
  //proves that we're in the database
 const db = client.db('sample_mflix')
 const moviesCollection = db.collection('movies')
 
 
 //console.log (await  moviesCollection.findOne({}))
-let query = {title:{$regex: /terminator/i}}//sonjer sa oui l'ami! itsearches for "terminator anywhere in the title and ignores case "
-let movieArray = await  moviesCollection.find(query).limit(3).toArray()//That's an array
+// let query = {title:{$regex: /legion/i}}//sonjer sa oui l'ami! itsearches for "terminator anywhere in the title and ignores case "
+// let movieArray = await  moviesCollection
+// .find(query)
+// //.limit(3)
+// .toArray()//That's an array
 
-for (let i=0; i < movieArray.length; i++){
-   console.log( movieArray[i].title)
+// for (let i=0; i < movieArray.length; i++){
+//    console.log( movieArray[i].title)
+// }
+// // console.log(movieArray[0])
+// // console.log
+// console.log(`there are ${movieArray.length} movies.`)
+
+// add a new movie 
+const newMovie = {
+    title: "The Boca Code story", 
+    rating: "R",
+    genre: "Comedy", 
+    releasedDate:"2022/12/16" 
 }
-// console.log(movieArray[0])
-// console.log
-console.log(`there are ${movieArray.length} movies.`)
+
+const results = await moviesCollection.insertOne(newMovie)
+console.log("Results of insert", results)
+
 
 
 
