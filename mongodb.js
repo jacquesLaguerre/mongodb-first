783c6f4bc3d2af2524789ccccdd9945b4f377d15
Jacques Laguerre <jacquesblaguerre@gmail.com>
2022-10-11 15:42:37 -0400

    searching movies

---
+++ mongodb.js
@@ -0,0 +1,23 @@
+import {MongoClient} from 'mongodb'
+
+import{uri } from "./credentials.js"
+
+const client = new MongoClient(uri)
+ //proves that we're in the database
+const db = client.db('sample_mflix')
+const moviesCollection = db.collection('movies')
+
+
+//console.log (await  moviesCollection.findOne({}))
+let query = {title:{$regex: /terminator/i}}//sonjer sa oui l'ami! itsearches for "terminator anywhere in the title and ignores case "
+let movieArray = await  moviesCollection.find(query).limit(3).toArray()//That's an array
+
+for (let i=0; i < movieArray.length; i++){
+   console.log( movieArray[i].title)
+}
+// console.log(movieArray[0])
+// console.log
+console.log(`there are ${movieArray.length} movies.`)
+
+
+
